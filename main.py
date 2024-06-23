2fb784273f1bb283a6a0964657ba6869cd060bc5
dmitri2309 <kerzhimanov@gmail.com>
2024-06-23 12:04:48 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,31 @@
+# задача длина слова
+a = "hello"
+print(len(a))
+
+# задача сумма и разности
+first=23
+second=12
+summa=first+second
+diff=first-second
+print(summa, diff)
+print(summa)
+print(diff)
+
+# задача среднее арифметическое
+first=23
+second=12
+third=7
+mean=(first+second+third)/3
+print(mean)
+
+# задача простые строчки
+first_string = "вторник"
+second_string = "понедельник, "
+print(second_string + first_string)
+
+# задача сложная формула
+a = 2
+b = 4
+c = 6
+f = (((a*b)+(a*c))^3)/2
+print(f)
