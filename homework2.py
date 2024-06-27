fdd04b0c797c4415c1f12b9401599384685f4fc7
dmitri2309 <kerzhimanov@gmail.com>
2024-06-27 16:22:23 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,6 @@
+Number_0f_implemented_homeworks = 12
+Number_of_spent_hours = 1.5
+Course_name = "Python"
+Hours_per_homework = Number_0f_implemented_homeworks/Number_of_spent_hours
+print('Курс:'+Course_name+', всего задач:'+str(Number_0f_implemented_homeworks)+\
+      ', затрачено часов:'+str(Number_of_spent_hours)+', среднее время выполнения '+str(Hours_per_homework)+' часов')
