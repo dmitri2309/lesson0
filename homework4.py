9ef6c0d7b4ca94756cdd1ce65ebf3abcabca2194
dmitri2309 <kerzhimanov@gmail.com>
2024-06-30 20:31:09 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,8 @@
+
+my_string= input('Введите любое выражение: ')
+print('в этом выражении',len(my_string), 'букв')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.capitalize())
+print(my_string.replace(' ',''))
+print(my_string[0].upper(), my_string[-1].upper())
